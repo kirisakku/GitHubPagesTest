54c7b003471d0712f2792a70e8b28553c986f72b
キリサク <41281605+kirisakku@users.noreply.github.com>
2021-08-17 00:24:01 +0900

    Create graph.js

---
+++ public/graph.js
@@ -0,0 +1,16 @@
+const ctx = document.getElementById('testChart');
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['8/16', '8/17', '8/18', '8/19', '8/20'],
+    datasets: [{
+      label: 'real',
+      data: [36, 36, 36, 36, 30],
+      borderColor: '#FF0000'
+    }, {
+      label: 'ideal',
+      data: [36, 27, 18, 9, 0],
+      borderColor: '#00FF00'
+    }]
+  }
+});
